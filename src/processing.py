54cae09c61b03304c4822bb90666c0e592e94d2d
natalia_yepifanova <epif97@mail.ru>
2024-11-07 10:10:03 +0300

    fixes by linters

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(dictionary_list: list, state:str = 'EXECUTED') -> list:
+def filter_by_state(dictionary_list: list, state: str = "EXECUTED") -> list:
     new_dictionary_list = list()
     for element in dictionary_list:
         if element["state"] == state:
@@ -6,6 +6,5 @@ def filter_by_state(dictionary_list: list, state:str = 'EXECUTED') -> list:
     return new_dictionary_list
 
 
-def sort_by_date(dictionary_list: list, type_of_sort:bool = True) -> list:
-   return sorted(dictionary_list, key=lambda dictionary: dictionary["date"], reverse = type_of_sort)
-
+def sort_by_date(dictionary_list: list, type_of_sort: bool = True) -> list:
+    return sorted(dictionary_list, key=lambda dictionary: dictionary["date"], reverse=type_of_sort)
