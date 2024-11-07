67b6a3f1199764ed9635269992b0e5a2025da4d9
natalia_yepifanova <epif97@mail.ru>
2024-11-07 10:42:11 +0300

    add readme.md and docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
 def filter_by_state(dictionary_list: list, state: str = "EXECUTED") -> list:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    соответствует указанному значению."""
     new_dictionary_list = list()
     for element in dictionary_list:
         if element["state"] == state:
@@ -7,4 +9,5 @@ def filter_by_state(dictionary_list: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(dictionary_list: list, type_of_sort: bool = True) -> list:
+    """Функция возвращает новый список, отсортированный по дате"""
     return sorted(dictionary_list, key=lambda dictionary: dictionary["date"], reverse=type_of_sort)
