bd3b59e6005cff821ea6c68915bf16231072e669
natalia_yepifanova <epif97@mail.ru>
2024-11-07 10:04:55 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -6,8 +6,6 @@ def filter_by_state(dictionary_list: list, state:str = 'EXECUTED') -> list:
     return new_dictionary_list
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state = 'CANCELED'))
+def sort_by_date(dictionary_list: list, type_of_sort:bool = True) -> list:
+   return sorted(dictionary_list, key=lambda dictionary: dictionary["date"], reverse = type_of_sort)
 
