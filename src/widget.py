96c1f1e49795130aa228c1124b769d171759692e
natalia_yepifanova <epif97@mail.ru>
2024-11-14 12:25:49 +0300

    add tests for widget

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,24 @@
-import masks
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card_info: str) -> str:
     """Функция маскирует номер карты или счета"""
+    if not isinstance(account_card_info, str) or account_card_info == '':
+        raise TypeError("Ошибка типа данных")
+
     new_account_card_info = account_card_info.split()
+
+    if not new_account_card_info[0].isalpha():
+        raise TypeError("Ошибка ввода данных, не хватает указания типа (Счет или название карты)")
+
     result_mask = ""
     if new_account_card_info[0] == "Счет":
-        result_mask = "Счет " + masks.get_mask_account(int(new_account_card_info[1]))
+        result_mask = "Счет " + get_mask_account(int(new_account_card_info[1]))
     else:
         name_of_card: list[str] = []
         for element in new_account_card_info:
             if element.isdigit():
-                result_mask = " ".join(name_of_card) + " " + masks.get_mask_card_number(int(element))
+                result_mask = " ".join(name_of_card) + " " + get_mask_card_number(int(element))
             else:
                 name_of_card.append(element)
     return result_mask
@@ -19,11 +26,11 @@ def mask_account_card(account_card_info: str) -> str:
 
 def get_date(date_format: str) -> str:
     """Функция возвращает дату в нужном формате"""
+    if not isinstance(date_format, str) or date_format == '':
+        raise TypeError("Ошибка типа данных")
+
+    if len(date_format) != 26:
+        raise ValueError('Ошибка в формате даты, должно быть 26 символов')
+
     new_date_format = date_format[8:10] + "." + date_format[5:7] + "." + date_format[:4]
     return new_date_format
-
-
-if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"))
-    print(get_date("2024-07-02T02:26:18.671407"))
---
+++ tests/test_widget.py
@@ -0,0 +1,63 @@
+from src.widget import mask_account_card, get_date
+import pytest
+
+
+@pytest.mark.parametrize('number, expected', [('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+                                              ('Счет 73654108430135874305', 'Счет **4305'),
+                                              ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
+                                              ('Счет 35383033474447895560', 'Счет **5560')])
+def test_mask_account_card(number, expected):
+    assert mask_account_card(number) == expected
+
+
+def test_mask_account_card_empty_input_data():
+    with pytest.raises(TypeError):
+        mask_account_card()
+
+
+def test_mask_account_card_empty_string():
+    with pytest.raises(TypeError):
+        mask_account_card('')
+
+
+def test_mask_account_card_wrong_type():
+    with pytest.raises(TypeError):
+        mask_account_card([2, 7, 8, 9])
+
+
+def test_mask_account_card_invalid_type_of_number():
+    with pytest.raises(TypeError):
+        mask_account_card('544555')
+
+
+@pytest.mark.parametrize('original_date, expected', [("2024-03-11T02:26:18.671407", "11.03.2024"),
+                                              ("2024-02-21T02:26:18.671407", '21.02.2024'),
+                                              ("2023-09-01T02:26:18.671407", '01.09.2023'),
+                                              ("2024-03-12T02:26:18.671407", '12.03.2024')])
+def test_get_date(original_date, expected):
+    assert get_date(original_date) == expected
+
+
+def test_get_date_len_of_date_after_twenty_six():
+    with pytest.raises(ValueError):
+        get_date("2024-03-11T02:26:18.67140755512")
+
+
+def test_get_date_len_of_date_less_than_twenty_six():
+    with pytest.raises(ValueError):
+        get_date("2024-03-11T12")
+
+
+def test_get_date_empty_input_data():
+    with pytest.raises(TypeError):
+        get_date()
+
+
+def test_get_date_empty_string():
+    with pytest.raises(TypeError):
+        get_date('')
+
+
+def test_get_date_wrong_type():
+    with pytest.raises(TypeError):
+        get_date(555)
