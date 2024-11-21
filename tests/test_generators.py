5817ef38e12c1da9b5101aa345ec6a1631e6f734
natalia_yepifanova <epif97@mail.ru>
2024-11-21 12:21:17 +0300

    add tests for card_number_generate

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,5 @@
 import pytest
-from src.generators import filter_by_currency, transaction_descriptions
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator, transactions
 
 
 def test_filter_by_currency_usd(transactions_list, usd_transactions):
@@ -24,6 +24,10 @@ def test_filter_by_currency_wrong_type():
         next(filter_by_currency(1, [4, 3, 2]))
     with pytest.raises(TypeError):
         next(filter_by_currency("some_sring", 2))
+    with pytest.raises(TypeError):
+        next(filter_by_currency(transactions, 2))
+    with pytest.raises(TypeError):
+        next(filter_by_currency(521, "USD"))
 
 
 def test_transaction_descriptions(transactions_list):
@@ -44,3 +48,39 @@ def test_transaction_descriptions_wrong_type():
     with pytest.raises(TypeError):
         next(transaction_descriptions("some_sring"))
 
+
+@pytest.mark.parametrize('start, stop, expected', [(10, 12, ["0000 0000 0000 0010",
+                                                             "0000 0000 0000 0011",
+                                                             "0000 0000 0000 0012"]),
+                                                   (5, 6, ["0000 0000 0000 0005",
+                                                           "0000 0000 0000 0006"]),
+                                                   (1000, 1002, ["0000 0000 0000 1000",
+                                                                 "0000 0000 0000 1001",
+                                                                 "0000 0000 0000 1002"])])
+def test_card_number_generator(start, stop, expected):
+    result = card_number_generator(start, stop)
+    assert next(result) == expected[0]
+    assert next(result) == expected[1]
+
+
+def test_card_number_generator_wrong_type():
+    with pytest.raises(TypeError):
+        next(card_number_generator([1, 2, 3], 1))
+    with pytest.raises(TypeError):
+        next(card_number_generator(5, "some_sring"))
+
+
+def test_card_number_generator_boundary_values():
+    result_max = card_number_generator(9999999999999998, 9999999999999999)
+    assert next(result_max) == "9999 9999 9999 9998"
+    assert next(result_max) == "9999 9999 9999 9999"
+
+
+def test_card_number_generator_after_boundary_values():
+    with pytest.raises(ValueError):
+        next(card_number_generator(10000000000000000, 10000000000000001))
+
+
+def test_card_number_generator_wrong_start_stop():
+    with pytest.raises(ValueError):
+        next(card_number_generator(7, 5))
