4bf2d1b6030d0acd87c1ec5e148b90aeb958db95
natalia_yepifanova <epif97@mail.ru>
2024-11-27 11:12:34 +0300

    add dec timing and start_end_func and fix by linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,38 +1,61 @@
-import os
 from functools import wraps
+from time import sleep, time
+from typing import Any, Callable
 
 
-def log(filename):
-    def inner(func):
+def log(filename: str | None = None) -> Callable:
+    """Декоратор, позволяющий логировать работу функции в текстовый файл/консоль"""
+
+    def inner(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 if filename is None or filename == "":
-                    print(
-                        f"{func.__name__} ok."
-                    )
+                    print(f"{func.__name__} ok.")
                 else:
-                    with open(filename, "a", encoding='utf-8') as log_file:
-                        log_file.write(
-                            f"{func.__name__} ok.\n"
-                        )
+                    with open(filename, "a", encoding="utf-8") as log_file:
+                        log_file.write(f"{func.__name__} ok.\n")
                 return result
             except Exception as er:
                 if filename is None or filename == "":
                     print(f"{func.__name__} error: {er}. Inputs: {args} {kwargs}")
                 else:
-                    with open(filename, "a", encoding='utf-8') as log_file:
+                    with open(filename, "a", encoding="utf-8") as log_file:
                         log_file.write(f"{func.__name__} error: {er}. Inputs: {args} {kwargs}\n")
+
         return wrapper
 
     return inner
 
+
+def start_end_function(func: Callable) -> Callable:
+    def wrapper(*args: Any, **kwargs: Any) -> None:
+        print(f"The beginning of the function: {func.__name__}")
+        func(*args, **kwargs)
+        print(f"The end of the function: {func.__name__}")
+
+    return wrapper
+
+
+def timing(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> None:
+        start_of_function = time()
+        func(*args, **kwargs)
+        sleep(1)
+        end_of_function = time()
+        print(f"Time execution: {end_of_function - start_of_function:.6f}")
+
+    return wrapper
+
+
 #
-# @log(filename="mylog.txt")
-# def my_function(x, y):
-#     return x + y
+@timing
+@start_end_function
+@log()
+def my_function(x: int | float, y: int | float) -> int | float:
+    return x + y
 
 
-# my_function(1, 2)  # my_function ok
-
+my_function(1, 2)  # my_function ok
