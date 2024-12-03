98423b9f4d5a51c1e813f1c180a05c7ac68210c0
natalia_yepifanova <epif97@mail.ru>
2024-12-03 14:57:35 +0300

    add func sum_of_transaction

---
+++ src/external_api.py
@@ -0,0 +1,43 @@
+import os
+from typing import Any, Dict
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+
+def sum_of_transaction(transaction: dict) -> Any[float]:
+    """Функция конвертирует валюту и возвращает сумму транзакции в рублях"""
+    transaction_amount = transaction.get("operationAmount").get("amount")
+    transaction_currency = transaction.get("operationAmount").get("currency").get("code")
+    if transaction_currency == "RUB":
+        return float(transaction_amount)
+    try:
+        if transaction_currency == "EUR" or transaction_currency == "USD":
+            url = (
+                f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from="
+                f"{transaction_currency}&amount={transaction_amount}"
+            )
+            headers = {"apikey": os.getenv("API_KEY")}
+            response = requests.request("GET", url, headers=headers, data={}, timeout=20, allow_redirects=False)
+            response.raise_for_status()
+            return float(response.json().get("result"))
+    except requests.exceptions.RequestException:
+        print("An error occurred. Please try again later.")
+
+
+if __name__ == "__main__":
+    print(
+        sum_of_transaction(
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "50", "currency": {"name": "руб.", "code": "EUR"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            }
+        )
+    )
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,23 @@
 import json
-from typing import List
+from typing import Any
 
 
-def financial_transactions(path: str) -> List[dict]:
+def financial_transactions(path: str) -> Any[list]:
+    """Функция возвращает список словарей с данными о финансовых транзакциях из JSON-файла"""
     try:
-        with open(path, encoding='UTF-8') as json_file:
+        with open(path, encoding="UTF-8") as json_file:
             transaction_data = json.load(json_file)
         return transaction_data
     except json.JSONDecodeError:
         print("Invalid JSON data.")
         return []
     except TypeError:
-        print("Object of type set is not JSON serializable.")
+        print("This object is not JSON serializable.")
         return []
     except FileNotFoundError:
         print("There is no such file")
         return []
 
 
-if __name__ == '__main__':
-    print(financial_transactions('../data/operations.json'))
+if __name__ == "__main__":
+    print(financial_transactions("../data/operations.json"))
